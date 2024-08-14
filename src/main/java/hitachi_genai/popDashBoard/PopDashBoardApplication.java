f6b2c77505a0b3f64a73db4ccf8faeb79ce032d6
mohedUsman <160305566+mohedUsman@users.noreply.github.com>
2024-08-14 11:59:45 +0530

    initial commit

---
+++ src/main/java/hitachi_genai/popDashBoard/PopDashBoardApplication.java
@@ -0,0 +1,13 @@
+package hitachi_genai.popDashBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopDashBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopDashBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hitachi_genai/popDashBoard/PopDashBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package hitachi_genai.popDashBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopDashBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
