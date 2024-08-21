3a2a0821b6a5a34a1facfc69f23c973be33aa3b6
piyushagarwal35 <piyush.agarwal@globallogic.com>
2024-08-21 19:21:19 +0530

    Added aggregation by service Category

--- src/main/java/hitachi_genai/popDashBoard/service/FocusExportService.java
+++ src/main/java/hitachi_genai/popDashBoard/service/FocusExportService.java
@@ -1,8 +1,13 @@
 package hitachi_genai.popDashBoard.service;
 
+import java.util.Date;
 import java.util.List;
 
 public interface FocusExportService {
-    public List<Object[]> getDailyCost();
+   // public List<Object[]> getDailyCost();
+
+    public List<Object[]> getCostForServiceCategory();
+
+    public List<Object[]> getCostForServiceCategory(Date chargePeriodStart, Date chargePeriodEnd);
 
 }
--- src/main/java/hitachi_genai/popDashBoard/service/FocusExportServiceImpl.java
+++ src/main/java/hitachi_genai/popDashBoard/service/FocusExportServiceImpl.java
@@ -4,6 +4,7 @@ import hitachi_genai.popDashBoard.Repository.FocusExportRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -11,8 +12,18 @@ public class FocusExportServiceImpl implements FocusExportService{
 
     @Autowired
     private FocusExportRepository focusExportRepository;
+//    @Override
+//    public List<Object[]> getDailyCost() {
+//        return focusExportRepository.findCost();
+//    }
+
     @Override
-    public List<Object[]> getDailyCost() {
-        return focusExportRepository.findCost();
+    public List<Object[]> getCostForServiceCategory() {
+        return focusExportRepository.findTotalCostForServiceCategory();
+    }
+
+    @Override
+    public List<Object[]> getCostForServiceCategory(Date chargePeriodStart, Date chargePeriodEnd) {
+        return focusExportRepository.findTotalCostForServiceCategoryCustomDate(chargePeriodStart,chargePeriodEnd);
     }
 }
